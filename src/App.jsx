86b5bb7261981c34a2bcd9c3dc9257f70bd3981e
unknown <alexli930220@gmail.com>
2024-07-21 00:24:41 -0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Hello, Super!</h1>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    historyApiFallback: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          "sass-loader"
+        ],
+      },
+    ],
+  },
+};
